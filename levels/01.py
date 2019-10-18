737c77747d07cdfdd20ceb7ad627d4cce0cb2889
bgarrido7 <bianevesgarrido@gmail.com>
2019-10-18 21:52:36 +0100

    demorei demasiado tempo só a dar prints :)

--- levels/01.py
+++ levels/01.py
@@ -1,2 +1,8 @@
-msg="Hello World!"
-print(msg)
+freq=0
+input_string=input("Enter a list element separated by space ")
+list=input_string.split()
+for x in list:
+    res=freq + int(x)
+    print("- Current frequency "+ str(freq) +", change of " + x +"; resulting frequency  "+ str(res))    
+    freq=res
+    
