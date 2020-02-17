71d382c4c875ea0d4ea765dec9ab198b16781c2f
bgarrido7 <bianevesgarrido@gmail.com>
2020-02-17 19:52:42 +0000

    day 2 done

--- lvl2/part2/02.py
+++ lvl2/part2/02.py
@@ -1,13 +1,18 @@
-f = open ('test.txt', 'r')
+f = open ('input.txt', 'r')
 array = []
-sum = 0
-
+sum=0
 for line in f:
     array.append([int(x) for x in line.split()])
 
+
 for x in array:
-    #print(x)
-    sum+=max(x)-min(x)
+
+    for i in range(len(x)-1):
+        for j in range(i+1, len(x)):
+            if max(x[i], x[j]) % min(x[i], x[j]) == 0:
+                #print(max(x[i], x[j]) / min(x[i], x[j]))
+                sum+=max(x[i], x[j]) / min(x[i], x[j])
+   
 
 print(sum)
 f.close()
