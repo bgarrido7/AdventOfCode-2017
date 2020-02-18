3ed3f24f78d1eafc2d3a43b975d0f1d5634e81e2
bgarrido7 <bianevesgarrido@gmail.com>
2020-02-18 12:54:22 +0000

    day 4 done

--- lvl3/03-part2.py
+++ lvl3/03-part2.py
@@ -3,12 +3,17 @@ import sys
 n = input()
 i = 1
 
-if n==i:
+if n==i:    #in center
     print(0)
     sys.exit()
 
-#def get_num():
-
+def get_num(start, finish, n_lines):
+    for x in range(start, finish, -1):     
+        if x==n:
+            middle = start - n_lines #number align with 1
+            step = abs(n - middle)
+            print(step + n_lines)
+            sys.exit()
 
 while(1):
     i+=2
@@ -21,35 +26,16 @@ while(1):
 
     elif row > n: 
         end = row-i+1
-        n_lines=(i-1)/2
+        lines = (i-1)/2
+
         #botom line
-        for x in range(row, end, -1):     
-            if x==n:
-                middle = row - n_lines #number align with 1
-                step = abs(n - middle)
-                print(step + n_lines)
-                sys.exit()
-        
-    
+        get_num(row, end, lines)
+
         #left line
-        for x in range(end, (end-i+1), -1):     
-            if x==n:
-                middle = end - n_lines #number align with 1
-                step = abs(n - middle)
-                print(step + n_lines)
-                sys.exit()
-                
+        get_num(end, end-i+1, lines)
+
         #top line
-            for x in range(end-i+1, (row-i*2-2), -1):     
-                if x==n:
-                    middle = end-i+1 - n_lines #number align with 1
-                    step = abs(n - middle)
-                    print(step + n_lines)
-                    sys.exit()
+        get_num(end-i+1, row-i*2-2, lines)
+        
          #right line
-            for x in range((row-i*2-2), (row-i*3), -1):     
-                if x==n:
-                    middle = (row-i*2-2) - n_lines #number align with 1
-                    step = abs(n - middle)
-                    print(step + n_lines)
-                    sys.exit()   
+        get_num(row-i*2-2, row-i*3, lines)
