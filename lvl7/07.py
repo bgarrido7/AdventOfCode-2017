f795a7eaba3f053125d28645a9a5e20eddd28c77
bgarrido7 <bianevesgarrido@gmail.com>
2020-03-01 17:22:37 +0000

    day 7 done

--- lvl7/07.py
+++ lvl7/07.py
@@ -1,48 +1,29 @@
 import re
 
 f = open("input.txt", 'r')
-this = True
-res = []
-followers = []
-follow = []
 candidates = []
-size=[]
-for content in f:
+father = []
 
-    line = re.split(' |, |\n', content)
+for content in f:
     child = []
-    if len(line)>3 :
-        child.append(line[0])
+    line = re.split(', |\n| ', content)
+
+    if len(line) > 3:
+        father.append(line[0])
         for i in range (3, len(line)-1):
             child.append(line[i])
+        candidates.append(list(child))
 
-        followers.append(list(child))
+#print father
+not_awnser = []
 
-for x in followers:
-    size.append(len(x))
-max_size=max(size)
+for check in candidates:
+    for test in check:
+        if test in father:
+            not_awnser.append(test)
 
-for z in followers:
-    if len(z) == max_size:
-        follow.append(z)
+#print not_awnser
 
-print(follow)
-
-for check in follow:
-    candidates.append(check[0])
-
-print "\ncandidates->", candidates, "\n"
-
-for check in follow:
-    this = True
-    for test in candidates:
-       # print(test)
-       # print(check)
-    
-        if test not in check:
-            #print("this is not the bottom")
-            this = False
-    if this==True :
-        res = check
-
-print(res[0])
+for test in father:
+    if test not in not_awnser:
+        print test
